ecc0e85e35a603027375bed70634947d8ee3ff61
Erik Verduin <everduin@cisco.com>
2025-07-04 14:48:12 -0500

    fix(main): #120 escape backticks
    fixed an issues where any text wrapped in `backticks` would be omitted from the message.
    
    Closes: #120

--- src/index.ts
+++ src/index.ts
@@ -449,7 +449,8 @@ function build_commit_string(
     const temp = commit_state.body.split("\\n"); // literal \n, not new-line.
     const res = temp
       .map((v) => (colorize ? color.reset(v.trim()) : v.trim()))
-      .join("\n");
+      .join("\n")
+      .replaceAll("`", "`");
     commit_string += colorize ? `\n\n${res}` : `\n\n${res}`;
   }
 
@@ -502,7 +503,7 @@ function build_commit_string(
   }
 
   if (escape_quotes) {
-    commit_string = commit_string.replaceAll('"', '\\"');
+    commit_string = commit_string.replaceAll('"', '\\"').replaceAll("`", "\\`");
   }
 
   return commit_string;
